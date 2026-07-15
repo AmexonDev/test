f5957a63135f1e0ebcb9a9c134c073b843332560
Amexon <jakubsoj13@gmail.com>
2026-07-15 16:52:47 +0200

    test

---
+++ app.js
@@ -0,0 +1,15 @@
+function App() {
+
+    const styles = {
+        main: {
+            padding: '20px'
+        }
+    }
+
+    return (
+        <div style={styles.main}>
+            <h1>Hello World!</h1>
+        </div>
+    )
+}
+
